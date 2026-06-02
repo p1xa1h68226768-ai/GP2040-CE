a7c3bf739de6f5e9c0f4b6e4817dd92e42833bed
pochipapa <p1xa1h68226768@gmail.com>
2026-06-02 12:25:34 +0900

    Update gp2040.cpp

--- src/gp2040.cpp
+++ src/gp2040.cpp
@@ -33,6 +33,7 @@
 #include "addons/gamepad_usb_host.h"
 #include "addons/he_trigger.h"
 #include "addons/tg16_input.h"
+#include "addons/negcon_input.h"
 
 // Pico includes
 #include "pico/bootrom.h"
@@ -116,6 +117,7 @@ void GP2040::setup() {
 	addons.LoadAddon(new RotaryEncoderInput());
 	addons.LoadAddon(new PCF8575Addon());
 	addons.LoadAddon(new TG16padInput());
+	addons.LoadAddon(new NeGconInput());
 
 	// Input override addons
 	addons.LoadAddon(new ReverseInput());
