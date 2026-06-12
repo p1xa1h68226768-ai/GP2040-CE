c68b7e323245e6e7b1b2d1bbbed8c7051968eb1a
pochipapa <p1xa1h68226768@gmail.com>
2026-06-12 20:40:38 +0900

    Clean up comments and formatting in negcon_input.h
    Removed commented Japanese text and adjusted formatting.

--- headers/addons/negcon_input.h
+++ headers/addons/negcon_input.h
@@ -10,14 +10,12 @@
 #define NEGCON_PIN_ATT 2
 #define NEGCON_PIN_CLK 3
 
-// システムのお約束（GPAddon）に従う宣言
 class NeGconInput : public GPAddon {
 public:
     virtual bool available() override;
     virtual void setup() override;
     virtual void process() override;
     virtual void preprocess() override {}
-    // ↓これが足りないと怒られていた「名前」の宣言です
     virtual std::string name() override { return "NeGconInput"; }
 
 private:
